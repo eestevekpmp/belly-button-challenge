06608b66131b734f1264080f334382d3058bfcf1
eestevekpmp <94544051+eestevekpmp@users.noreply.github.com>
2023-04-30 16:24:11 -0400

    95% Code
    Bar and Bubble Charts working. Need to check Github pages and perform final clean/comment.

--- static/js/app.js
+++ static/js/app.js
@@ -2,12 +2,6 @@ let data = d3.json("samples.json");
 console.log(data);
 
 
-
-
-
-// function that updates the dashboard
-
-
 // function that populates the metadata
 function demoInfo(sample){
     //console.log(sample);
@@ -47,34 +41,32 @@ function buildBarChart(sample){
     d3.json("samples.json").then((data) => {
         // retrieve all sample data
         let sampleData = data.samples;
-        //console.log(sampleData);
+       
         
         // filter with sample value
         let result = sampleData.filter(sampleResult => sampleResult.id == sample);
-        //console.log(result);
+        
         
         // access index 0 from the array
         let resultData = result[0];
-        //console.log(resultData);
+       
 
         // get the otu ids and labels
         let otu_ids = resultData.otu_ids;
         let otu_labels = resultData.otu_labels;
         let sample_values = resultData.sample_values;
-        //console.log(otu_ids);
-        //console.log(otu_labels);
-        //console.log(sample_values);
+       
 
         // build bar chart
         let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`);
         let xValues = sample_values.slice(0,10);
         let textLabels = otu_labels.slice(0,10);
-        //console.log(textLabels);
+        
 
         let barChart = {
-            y: yticks,
-            x: xValues,
-            text: textLabels,
+            y: yticks.reverse(),
+            x: xValues.reverse(),
+            text: textLabels.reverse(),
             type: "bar",
             orientation: "h"
         }
@@ -86,10 +78,59 @@ function buildBarChart(sample){
         Plotly.newPlot("bar", [barChart], layout);
 
 
-        
+
     });
     
 }
+
+// function that builds the bubble chart
+function buildBubbleChart(sample){
+
+
+    d3.json("samples.json").then((data) => {
+        // retrieve all sample data
+        let sampleData = data.samples;
+       
+        
+        // filter with sample value
+        let result = sampleData.filter(sampleResult => sampleResult.id == sample);
+        
+        
+        // access index 0 from the array
+        let resultData = result[0];
+       
+
+        // get the otu ids and labels
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+       
+
+        // build bubble chart
+        
+        let bubbleChart = {
+            y: sample_values,
+            x: otu_ids,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }
+
+        let layout = {
+            title: "Bacteria Cultures Per Sample",
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"}
+        };
+
+        Plotly.newPlot("bubble", [bubbleChart], layout);
+    });
+}
+
+
 // function that initializes the dashboard
 function initialize(){
     // load the data from the .json file
@@ -115,12 +156,12 @@ function initialize(){
 
     // call function to build metadata
     demoInfo(sample1);
-    // call functin to build bar chart
+    // call function to build bar chart
     buildBarChart(sample1);
+    // call function to build bubble chart
+    buildBubbleChart(sample1);
     });
     
-
-
 }
 // function that updates the dashboard
 function optionChanged(item){
@@ -128,6 +169,8 @@ function optionChanged(item){
     demoInfo(item);
     // call function to build the bar chart
     buildBarChart(item);
+    // call function to build the bubble chart
+    buildBubbleChart(item);
 }
 // call the initialize function
 initialize();
